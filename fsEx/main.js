ac62effe03e5a1b153ae2dccecbf10d038322e4c
Sean Pollock <seanpollock3344@gmail.com>
2016-04-03 19:58:52 -0500

    fs read file example

--- fsEx/main.js
+++ fsEx/main.js
@@ -4,7 +4,15 @@ var fs = require('fs');
 
 // console.log(data.toString());
 
-fs.writeFile('message.txt', 'hello world', function(err){
-	if (err) throw err;
-	console.log("file should be saved")
+// fs.writeFile('message.txt', 'hello world', function(err){
+// 	if (err) throw err;
+// 	console.log("file should be saved")
+// });
+
+fs.readFile('message.txt', function(err, data) {
+	if (err) {
+		console.log("hitting an error");
+		return;
+	}
+	console.log(data.toString());
 });
