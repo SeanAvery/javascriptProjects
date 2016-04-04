6907e266953fcb5ba7b94e381d2d2add8c869515
Sean Pollock <seanpollock3344@gmail.com>
2016-04-04 18:20:11 -0500

    working with event emitter

---
+++ eventEmitter.js
@@ -0,0 +1,34 @@
+var events = require('events');
+
+// create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// create two listeners
+var listener1 = function listener1() {
+	console.log('listener1 executed');
+}
+var listener2 = function listener2() {
+	console.log('listener2 executed');
+}
+
+// Bind the connection event with the listen function
+eventEmitter.on('connection', listener1);
+eventEmitter.on('connection', listener2);
+
+// get the number of event listeners
+var numEventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log(numEventListeners + " Listeners listening to connection event");
+ 
+// fire the connection event
+eventEmitter.emit('connection');
+
+// remove the binding for listener1
+eventEmitter.removeListener('connection', listener1);
+console.log("removed listener 1");
+
+// fire the connection event
+eventEmitter.emit('connection');
+
+// check num of listeners
+var numEventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log(numEventListeners + " Listeners listening to connection event this time");
