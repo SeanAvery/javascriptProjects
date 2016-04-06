49e8ec38bf53de0a5587003d60872f42a78103d6
Sean Pollock <seanpollock3344@gmail.com>
2016-04-05 23:48:47 -0500

    fs readFile, writeFile

--- streamEx/async.js
+++ streamEx/async.js
@@ -11,3 +11,35 @@ fs.readFile('input.txt', function(err, data){
 // syncronous way to read file
 var file = fs.readFileSync('input.txt');
 console.log("syncrounous output: " + file.toString());
+
+
+// open a file in local file structure 
+fs.open('input.txt', 'r+', function(err, data){
+	if(err) {
+		return console.log(err);
+	}
+});
+
+// get file information with fs.stat
+fs.stat('input.txt', function(err, data){
+	if (err) {
+		return console.log(err);
+	}
+	console.log(data);
+	console.log("File: " + data.isFile());
+	console.log("Directory: " + data.isDirectory());
+});
+
+// write a file with some text
+fs.writeFile('message.txt', 'hellloooo world', function(err, data){
+	if (err) {
+		return console.log(err);
+	}
+});
+
+fs.readFile('message.txt', function(err, data){
+	if (err) {
+		return console.log(err);
+	}
+	console.log(data.toString());
+});
