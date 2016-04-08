064549335b4132d21ca468c45859a628b1278b49
Sean Pollock <seanpollock3344@gmail.com>
2016-04-07 20:35:09 -0500

    simple express server

--- express/main.js
+++ express/main.js
@@ -1,8 +1,24 @@
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function(req, res){
+// 	res.send('hello world');
+// });
+
+// app.listen(3000);
+
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('hello world');
+app.get('/', function(req, res) {
+	res.send('hello worldddddd');
 });
 
-app.listen(3000);
+var server = app.listen('3000', function(req, res) {
+
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log(host);
+	console.log(port);
+	console.log("http" + host + port);
+});
