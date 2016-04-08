bbbb4830d13075611ce62a82871fb09d4ee532be
Sean Pollock <seanpollock3344@gmail.com>
2016-04-08 15:10:44 -0500

    express server: input to json format

--- express/staticMain.js
+++ express/staticMain.js
@@ -9,14 +9,25 @@ app.get('/', function(req, res) {
 	res.send('hello world');
 });
 
-app.get('/index.html', function(reg, res) {
+app.get('/index.html', function(req, res) {
 	res.sendFile(__dirname, + '/' + 'index.html');
 });
 
+app.get('/process_get', function(req, res){
+	// prepare resonse in Json format
+	var response = {
+		firstname : req.query.firstName,
+		lastname : req.query.lastName
+
+	};
+	console.log(response);
+})
 var server = app.listen('3000', function() {
 	var host = server.address().address;
 	var port = server.address().address;
 });
+
+
 
 
 
